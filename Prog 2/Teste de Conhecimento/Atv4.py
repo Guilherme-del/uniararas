21d8db6937b393d174c24ae9d25786fe7acb525a
Guilherme Cavenaghi <guilherme_cavenaghi@hotmail.com>
2021-04-17 10:40:50 -0300

    Add files via upload

---
+++ Prog 2/Teste de Conhecimento/Atv4.py
@@ -0,0 +1,13 @@
+print('='*69)
+print('\t\t \t SCHOOLNET - ATV4')
+print('='*69)
+
+print('Bem vindo ao calculator FHO')
+
+print ('Insira o valor de x')
+x = float(input())
+y = ((x**3) + ((3*(x+2)/4)))
+              
+print ('o valor de y é: ',y)
+
+                    
