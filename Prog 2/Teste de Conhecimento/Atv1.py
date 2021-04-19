1234db8f2c1f259c8ab26e1eaf6befa7335bd76f
Guilherme Cavenaghi <guilherme_cavenaghi@hotmail.com>
2021-04-18 23:28:55 -0300

    Delete Prog 2 directory

--- Prog 2/Teste de Conhecimento/Atv1.py
+++
@@ -1,5 +0,0 @@
-print('='*69)
-print('\t\t \t SCHOOLNET - ATV1')
-print('='*69)
-
-print('Seja Bem vindo')
--- Prog 2/Teste de Conhecimento/Atv2.py
+++
@@ -1,7 +0,0 @@
-print('='*35)
-print('\t\t SCHOOLNET - ATV2')
-print('='*35)
-
-print('Seja Bem vindo')
-nome = str(input('Qual seu nome?'))
-print('Seu nome é: ',nome)
