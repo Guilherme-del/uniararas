47b2f8bf5f50f74657c8dc41592e52ac50a5ab97
Guilherme Cavenaghi <guilherme_cavenaghi@hotmail.com>
2021-06-03 17:32:00 -0300

    teste 5

--- Prog 2/5Teste/8.py
+++ Prog 2/5Teste/8.py
@@ -1,10 +1,40 @@
 print ("="*69)
 print ("\t \t \t Teste de conhecimento 5 - Exercicio 8")
 print ("="*69)
+import time
 
-itens = ['Litrão Aguado', 10.00 , 'Catuaba Selvagem' , 15.00 , "Corotin" , 4.00 ]
+print('='*45)
+print('Novo sistema o Boteco do Pai')
+print('='*45)
 
-for c,v in enumerate (itens) : 
-    print('valor',v,'esta na posição', c+1) 
+lista=[]
 
+itens = [10.00 ,15.00 , 4.00]
+
+for i in range (1):
+    print('Item: litrão aguado')
+    print('Preço antigo:R$10.00')
+    nv=float(input('Informe o novo valor:  '))
+    lista.append(nv)
+    
+    time.sleep(1)
+    
+    print('Item: Catuaba selvagem')
+    print('Preço antigo:R$15.00')
+    nv=float(input('Informe o novo valor:  '))
+    lista.append(nv)
+    
+    time.sleep(1)
+    
+    print('Item: Corotin')
+    print('Preço antigo:R$4.00')
+    nv=float(input('Informe o novo valor:  '))
+    lista.append(nv)
+    
+for i ,v in enumerate(lista):
+    print('='*45)
+    print('Novos valores:  ')
+    print('Litrão aguado,Catuaba Selvagem, Corotin ')
+    print('R$' ,lista)
+    break
  
