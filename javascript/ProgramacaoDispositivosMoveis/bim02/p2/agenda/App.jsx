60d700a7da89a4c5684437eb65c0ab84650fdb18
Guilherme-del <guilherme_cavenaghi@hotmail.com>
2023-11-20 03:42:56 -0300

    feat: Added SplashScreen

--- javascript/ProgramacaoDispositivosMoveis/bim02/p2/agenda/App.jsx
+++ javascript/ProgramacaoDispositivosMoveis/bim02/p2/agenda/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import { View  } from 'react-native';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootStackScreen from './src/components/navigator/stackScreen';
 import Toast from 'react-native-toast-message';
-import {toastConfig } from './src/components/toastConfig/toastify';
+import { toastConfig } from './src/components/toastConfig/toastify';
+import SplashScreen from 'react-native-splash-screen'
+
+
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <Toast config={toastConfig} />
