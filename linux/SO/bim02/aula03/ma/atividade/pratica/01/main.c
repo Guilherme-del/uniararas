f15e9df2b6a06b689f9515a146df0632c5377dbd
Guilherme-del <guilherme_cavenaghi@hotmail.com>
2023-11-23 19:45:44 -0300

    feat : teorico

--- linux/SO/bim02/aula03/ma/atividade/pratica/01/main.c
+++ linux/SO/bim02/aula03/ma/atividade/pratica/01/main.c
@@ -1,7 +1,7 @@
 /*
-Em sistemas operacionais baseados em Unix/Linux, 
-pode-se usar a função fork() para criar um processo filho e execvp() para 
-executar o comando desejado. 
+Em sistemas operacionais baseados em Unix/Linux,
+pode-se usar a função fork() para criar um processo filho e execvp() para
+executar o comando desejado.
 */
 
 #include <stdio.h>
@@ -9,21 +9,26 @@ executar o comando desejado.
 #include <unistd.h>
 #include <sys/types.h>
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
         fprintf(stderr, "Uso: %s <diretório>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     pid_t pid = fork();
 
-    if (pid == -1) {
+    if (pid == -1)
+    {
         perror("Erro ao criar processo filho");
         exit(EXIT_FAILURE);
     }
 
-    if (pid == 0) {  // Código executado pelo processo filho
-        if (chdir(argv[1]) == -1) {
+    if (pid == 0)
+    { // Código executado pelo processo filho
+        if (chdir(argv[1]) == -1)
+        {
             perror("Erro ao executar o comando cd");
             exit(EXIT_FAILURE);
         }
@@ -31,14 +36,19 @@ int main(int argc, char *argv[]) {
         // Se você quiser realizar mais ações no processo filho após o cd, adicione-as aqui
 
         exit(EXIT_SUCCESS);
-    } else {  // Código executado pelo processo pai
+    }
+    else
+    { // Código executado pelo processo pai
         // Aguardar o processo filho terminar
         int status;
         waitpid(pid, &status, 0);
 
-        if (WIFEXITED(status)) {
+        if (WIFEXITED(status))
+        {
             printf("Processo filho terminou com código de saída %d\n", WEXITSTATUS(status));
-        } else {
+        }
+        else
+        {
             printf("Processo filho terminou anormalmente\n");
         }
     }
