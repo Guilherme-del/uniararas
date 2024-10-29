3d30ace95bdb0901addd21e448f3748ae3c43bcb
Guilherme-del <guilherme_cavenaghi@hotmail.com>
2024-10-29 18:12:24 -0300

    refactor: Update database configuration and docker-compose.yml
    - Update the localUrl in the database configuration to include replica set for MongoDB.
    - Update the docker-compose.yml file to include MongoDB replica services.
    - Add keyfile for MongoDB replica services in the docker-compose.yml file.
    - Remove unused keyfile from the repository.

--- javascript/ProgramacaoParalelaDistribuida/backEnd/src/config/database.js
+++ javascript/ProgramacaoParalelaDistribuida/backEnd/src/config/database.js
@@ -1,20 +1,6 @@
-/**
- * Arquivo: config/database.js
- * Descrição: arquivo responsável pelas 'connectionStrings da aplicação: MongoDb.
- * Data: 23/06/2024
- * Author: Guilherme Cavenaghi
- */
-
 module.exports = {
   local:
   {
-    localUrl: 'mongodb://root:password@mongo:27017/meancrud?replicaSet=rs0',
+    localUrl: 'mongodb://root:password@mongo:27017,mongo_replica_1:27017,mongo_replica_2:27017/meancrud?replicaSet=rs0',
   },
 };
-
-/**
- * Arquivo: src/routes/funcionarioRoute.js
- * Descrição: arquivo responsável pelas rotas da api relacionado ao Funcionário.
- * Data: 23/06/2024
- * Author: Guilherme Cavenaghi
- */
