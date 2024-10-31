b41688fb6a5d7ba1e2a443ddbef08ae78decfa9e
Guilherme-del <guilherme_cavenaghi@hotmail.com>
2024-10-31 15:52:19 -0300

    feat: Implement spaceship rendering with detailed body and tail, and add star background

---
+++ python/compGrafica/bim02/jato/main.py
@@ -0,0 +1,626 @@
+# Nome: Enzo Barbosa das Neves
+# RA: 107501
+
+# CONTROLES DE MOVIMENTO DA CAMERA:
+    # Setas (Direita, Esquerda, Cima, Baixo)
+    # PageUp e PageDown
+    # Home
+    # End
+
+# CONTROLE DE MOVIMENTO DO CARRO:
+    # W - move para a frente, depende da posição da asa traseira sobe ou desce
+    # 4 - rotaciona asa vertical para esquerda
+    # 6 - rotaciona asa vertical para direita
+    # 8 - rotaciona asa horizontal para baixo
+    # 5 - rotaciona asa horizontal para cima
+
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from OpenGL.GL import *
+import sys
+
+name = 'F-35'
+especularidade = [1.0,1.0,1.0,1.0]
+especMaterial = 60
+px = 30
+py = -30
+pz = 0
+direcao = 0
+faixa1 = 40
+faixa2 = -40
+rotacaoy = 0
+subir = 0
+
+def drawF35():
+
+    glRotatef(px, 1, 0, 0) # Rotaciona em relação ao eixo X (Setas cima e baixo)
+    glRotatef(py, 0, 1, 0) # Rotaciona em relação ao eixo Y (Setas direita e esquerda)
+    glRotatef(pz, 0, 0, 1) # Rotaciona em relação ao eixo Z (teclas PgUp e PgDn)
+    glTranslatef(0, subir, 0)
+#----------------------------------------------------------------------------------------------
+
+    glTranslatef(0, 0, -7)
+    glColor3f(0.38,0.38,0.38)
+    glTranslatef(0, 1.25, -1)
+    gluCylinder(gluNewQuadric(), 1.25, 1.25, 14, 16, 16)
+    glTranslatef(0, -1.25, 1)
+    glTranslatef(0, 1.25, 13)
+    gluCylinder(gluNewQuadric(), 1.25, 0.75, 2, 16, 16)
+    glTranslatef(0, -1.25, -13)
+    glTranslatef(0, 1.25, 15)
+    gluCylinder(gluNewQuadric(), 0.75, 0.2, 1, 16, 16)
+    glTranslatef(0, -1.25, -15)
+    glTranslatef(0, 1.25, 16)
+    gluCylinder(gluNewQuadric(), 0.2, 0, 0.25, 16, 16)
+    glTranslatef(0, -1.25, -15)
+    glColor3f(0.15,0.15,0.15)
+    glTranslatef(0, 1.25, -3)
+    gluCylinder(gluNewQuadric(), 0.5, 1.25, 1, 16, 16)
+    glTranslatef(0, -1.25, 3)
+    glColor3f(1,0.6,0.3)
+    glTranslatef(0, 1.25, -1)
+    gluSphere(gluNewQuadric(), 1.25, 8, 8)
+    glTranslatef(0, -1.25, 1)
+    #janela
+    glColor3f(0.8,0.8,1)
+    glTranslatef(0, 2.2, 13)
+    gluSphere(gluNewQuadric(), 0.6, 8, 8)
+    glTranslatef(0, -2.2, -13)
+    glTranslatef(0, 2.5, 11)
+    gluSphere(gluNewQuadric(), 0.6, 8, 8)
+    glTranslatef(0, -2.5, -11)
+    glTranslatef(0, 2.5, 11)
+    glRotatef(8, 1, 0, 0)
+    gluCylinder(gluNewQuadric(), 0.57, 0.57, 2.1, 16, 16)
+    glRotatef(-8, 1, 0, 0)
+    glTranslatef(0, -2.5, -11)
+    #asa direita
+    glColor3f(0.4, 0.4, 0.4)
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.36, 1.32, 6.72),
+    glVertex3f(-2.36, 1.32, 0.0),
+    glVertex3f(-7.44, 1.29, 1.26),
+    glVertex3f(-7.44, 1.29, 3.36),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.32, 1.05, 0.0),
+    glVertex3f(-2.32, 1.05, 6.72),
+    glVertex3f(-7.44, 1.05, 3.36),
+    glVertex3f(-7.44, 1.05, 1.26),
+    glEnd()
+    glColor3f(0.3, 0.3, 0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.32, 1.05, 6.72),
+    glVertex3f(-2.36, 1.32, 6.72),
+    glVertex3f(-7.44, 1.29, 3.36),
+    glVertex3f(-7.44, 1.05, 3.36),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-7.44, 1.05, 1.26),
+    glVertex3f(-7.44, 1.05, 3.36),
+    glVertex3f(-7.44, 1.29, 3.36),
+    glVertex3f(-7.44, 1.29, 1.26),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-7.44, 1.05, 1.26),
+    glVertex3f(-7.44, 1.29, 1.26),
+    glVertex3f(-2.36, 1.32, 0.0),
+    glVertex3f(-2.32, 1.05, 0.0),
+    glEnd()
+    #Asa esquerda
+    glColor3f(0.4, 0.4, 0.4)
+    glBegin(GL_POLYGON)
+    glVertex3f(2.36, 1.32, 6.72),
+    glVertex3f(7.44, 1.29, 3.36),
+    glVertex3f(7.44, 1.29, 1.26),
+    glVertex3f(2.36, 1.32, 0.0),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(2.32, 1.05, 0.0),
+    glVertex3f(7.44, 1.05, 1.26),
+    glVertex3f(7.44, 1.05, 3.36),
+    glVertex3f(2.32, 1.05, 6.72),
+    glEnd()
+    glColor3f(0.3, 0.3, 0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(2.32, 1.05, 6.72),
+    glVertex3f(7.44, 1.05, 3.36),
+    glVertex3f(7.44, 1.29, 3.36),
+    glVertex3f(2.36, 1.32, 6.72),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(7.44, 1.05, 1.26),
+    glVertex3f(7.44, 1.29, 1.26),
+    glVertex3f(7.44, 1.29, 3.36),
+    glVertex3f(7.44, 1.05, 3.36),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(7.44, 1.05, 1.26),
+    glVertex3f(2.32, 1.05, 0.0),
+    glVertex3f(2.36, 1.32, 0.0),
+    glVertex3f(7.44, 1.29, 1.26),
+    glEnd()
+    #base
+    glColor3f(0.3, 0.3, 0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(-1.4, 1.32, 11.4),
+    glVertex3f(-1.16, 1.61, 12.04),
+    glVertex3f(-1.16, 1.61, -1.7),
+    glVertex3f(-1.4, 1.32, -1.7),
+    glEnd()
+    glColor3f(0.2, 0.2, 0.2)
+    glBegin(GL_POLYGON)
+    glVertex3f(-1.4, 1.32, 11.4),
+    glVertex3f(-1.16, 1.61, 12.04),
+    glVertex3f(-1.16, 1.32, 11.4),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(1.4, 1.32, 11.4),
+    glVertex3f(1.16, 1.61, 12.04),
+    glVertex3f(1.16, 1.32, 11.4),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.36, 1.32, -1.7),
+    glVertex3f(-2.36, 1.12, -1.7),
+    glVertex3f(-1.4, 1.12, -1.7),
+    glVertex3f(-1.4, 1.32, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(2.36, 1.32, -1.7),
+    glVertex3f(2.36, 1.12, -1.7),
+    glVertex3f(1.4, 1.12, -1.7),
+    glVertex3f(1.4, 1.32, -1.7),
+    glEnd()
+    glColor3f(0.3, 0.3, 0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(1.4, 1.32, 11.4),
+    glVertex3f(1.4, 1.32, -1.7),
+    glVertex3f(1.16, 1.61, -1.7),
+    glVertex3f(1.16, 1.61, 12.04),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-1.4, 1.32, -1.7),
+    glVertex3f(-1.16, 1.61, -1.7),
+    glVertex3f(-0.92, 1.12, -1.7),
+    glVertex3f(-1.4, 1.12, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-1.4, 1.32, -1.7),
+    glVertex3f(-1.16, 1.61, -1.7),
+    glVertex3f(-0.92, 1.12, -1.7),
+    glVertex3f(-1.4, 1.12, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(1.4, 1.32, -1.7),
+    glVertex3f(1.4, 1.12, -1.7),
+    glVertex3f(0.92, 1.12, -1.7),
+    glVertex3f(1.16, 1.61, -1.7),
+    glEnd()
+    glColor3f(0.35,0.35,0.35)
+    glBegin(GL_POLYGON)
+    glVertex3f(-1.4, 1.32, 11.4),
+    glVertex3f(-1.4, 1.32, -1.7),
+    glVertex3f(-2.36, 1.32, -1.7),
+    glVertex3f(-2.36, 1.32, 11.4),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(1.4, 1.32, 11.4),
+    glVertex3f(2.36, 1.32, 11.4),
+    glVertex3f(2.36, 1.32, -1.7),
+    glVertex3f(1.4, 1.32, -1.7),
+    glEnd()
+    glColor3f(0.3,0.3,0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.0, 0.0, 0.0),
+    glVertex3f(-2.32, 1.12, -1.7),
+    glVertex3f(-0.92, 1.12, -1.7),
+    glVertex3f(-0.6, 0.0, 0.0),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.36, 1.32, 0.0),
+    glVertex3f(-2.36, 1.32, -1.7),
+    glVertex3f(-2.32, 1.12, -1.7),
+    glVertex3f(-2.0, 0.0, 0.0),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(2.0, 0.0, 0.0),
+    glVertex3f(0.6, 0.0, 0.0),
+    glVertex3f(0.92, 1.12, -1.7),
+    glVertex3f(2.32, 1.12, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(2.36, 1.32, 0.0),
+    glVertex3f(2.0, 0.0, 0.0),
+    glVertex3f(2.32, 1.12, -1.7),
+    glVertex3f(2.36, 1.32, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.36, 1.32, 11.4),
+    glVertex3f(-2.36, 1.32, 0.0),
+    glVertex3f(-2.0, 0.0, 0.0),
+    glVertex3f(-2.0, 0.0, 10.5),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(2.36, 1.32, 11.4),
+    glVertex3f(2.0, 0.0, 10.5),
+    glVertex3f(2.0, 0.0, 0.0),
+    glVertex3f(2.36, 1.32, 0.0),
+    glEnd()
+    glColor3f(0.2,0.2,0.2)
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.0, 0.0, 10.5),
+    glVertex3f(2.0, 0.0, 10.5),
+    glVertex3f(2.0, 0.0, 0.0),
+    glVertex3f(-2.0, 0.0, 0.0),
+    glEnd()
+    glColor3f(0.1, 0.1, 0.1)
+    glBegin(GL_POLYGON)
+    glVertex3f(2.36, 1.32, 11.4),
+    glVertex3f(-2.36, 1.32, 11.4),
+    glVertex3f(-2.0, 0.0, 10.5),
+    glVertex3f(2.0, 0.0, 10.5),
+    glEnd()  
+    #asa traseira direita
+    glTranslatef(0, 1.22, -1.7)
+    glRotate(rotacaoy, 1, 0, 0)
+    glTranslatef(0, -1.22, 1.7)
+    glColor3f(0.4, 0.4, 0.4)
+    glBegin(GL_POLYGON)
+    glVertex3f(-1.4, 1.12, -1.7),
+    glVertex3f(-1.4, 1.12, -3.84),
+    glVertex3f(-4.45, 1.12, -3.84),
+    glVertex3f(-4.45, 1.12, -2.64),
+    glVertex3f(-2.36, 1.12, -1.7),  
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-1.4, 1.32, -1.7),
+    glVertex3f(-1.4, 1.32, -3.84),
+    glVertex3f(-4.45, 1.32, -3.84),
+    glVertex3f(-4.45, 1.32, -2.64),
+    glVertex3f(-2.36, 1.32, -1.7),  
+    glEnd()
+    glColor3f(0.3, 0.3, 0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.36, 1.32, -1.7),
+    glVertex3f(-2.36, 1.12, -1.7),
+    glVertex3f(-1.4, 1.12, -1.7),
+    glVertex3f(-1.4, 1.32, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-2.36, 1.32, -1.7),
+    glVertex3f(-4.45, 1.32, -2.64),
+    glVertex3f(-4.45, 1.12, -2.64),
+    glVertex3f(-2.36, 1.12, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-4.45, 1.12, -3.84),
+    glVertex3f(-4.45, 1.12, -2.64),
+    glVertex3f(-4.45, 1.32, -2.64),
+    glVertex3f(-4.45, 1.32, -3.84),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-4.45, 1.12, -3.84),
+    glVertex3f(-4.45, 1.32, -3.84),
+    glVertex3f(-1.4, 1.32, -3.84),
+    glVertex3f(-1.4, 1.12, -3.84),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-1.4, 1.12, -3.84),
+    glVertex3f(-1.4, 1.32, -3.84),
+    glVertex3f(-1.4, 1.32, -1.7),
+    glVertex3f(-1.4, 1.12, -1.7),
+    glEnd()
+    glTranslatef(0, 1.22, -1.7)
+    glRotate(-rotacaoy, 1, 0, 0)
+    glTranslatef(0, -1.22, 1.7)
+    #asa traseira esquerda
+    glTranslatef(0, 1.22, -1.7)
+    glRotate(rotacaoy, 1, 0, 0)
+    glTranslatef(0, -1.22, 1.7)
+    glColor3f(0.4, 0.4, 0.4)
+    glBegin(GL_POLYGON)
+    glVertex3f(1.4, 1.12, -1.7),
+    glVertex3f(1.4, 1.12, -3.84),
+    glVertex3f(4.45, 1.12, -3.84),
+    glVertex3f(4.45, 1.12, -2.64),
+    glVertex3f(2.36, 1.12, -1.7),  
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(1.4, 1.32, -1.7),
+    glVertex3f(1.4, 1.32, -3.84),
+    glVertex3f(4.45, 1.32, -3.84),
+    glVertex3f(4.45, 1.32, -2.64),
+    glVertex3f(2.36, 1.32, -1.7),  
+    glEnd()
+    glColor3f(0.3, 0.3, 0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(2.36, 1.32, -1.7),
+    glVertex3f(2.36, 1.12, -1.7),
+    glVertex3f(1.4, 1.12, -1.7),
+    glVertex3f(1.4, 1.32, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(2.36, 1.32, -1.7),
+    glVertex3f(4.45, 1.32, -2.64),
+    glVertex3f(4.45, 1.12, -2.64),
+    glVertex3f(2.36, 1.12, -1.7),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(4.45, 1.12, -3.84),
+    glVertex3f(4.45, 1.12, -2.64),
+    glVertex3f(4.45, 1.32, -2.64),
+    glVertex3f(4.45, 1.32, -3.84),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(4.45, 1.12, -3.84),
+    glVertex3f(4.45, 1.32, -3.84),
+    glVertex3f(1.4, 1.32, -3.84),
+    glVertex3f(1.4, 1.12, -3.84),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(1.4, 1.12, -3.84),
+    glVertex3f(1.4, 1.32, -3.84),
+    glVertex3f(1.4, 1.32, -1.7),
+    glVertex3f(1.4, 1.12, -1.7),
+    glEnd()
+    glTranslatef(0, 1.22, -1.7)
+    glRotate(-rotacaoy, 1, 0, 0)
+    glTranslatef(0, -1.22, 1.7)
+    #asa traseira vertical esquerda
+    glTranslatef(1.85, 1.25, 0)
+    glRotatef(-10, 0, 0, 1)
+    glRotatef(direcao, 0, 1, 0)
+    glColor3f(0.4, 0.4, 0.4)
+    glBegin(GL_POLYGON)
+    glVertex3f(0.1, 0, 1.5),
+    glVertex3f(0.1, 0, -1.5),
+    glVertex3f(0.1, 3, -2),
+    glVertex3f(0.1, 3, -0.5),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 0, 1.5),
+    glVertex3f(-0.1, 0, -1.5),
+    glVertex3f(-0.1, 3, -2),
+    glVertex3f(-0.1, 3, -0.5),
+    glEnd()
+    glColor3f(0.3, 0.3, 0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 0, 1.5),
+    glVertex3f(-0.1, 3, -0.5),
+    glVertex3f(0.1, 3, -0.5),
+    glVertex3f(0.1, 0, 1.5),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 3, -0.5),
+    glVertex3f(0.1, 3, -0.5),
+    glVertex3f(0.1, 3, -2),
+    glVertex3f(-0.1, 3, -2),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 0, -1.5),
+    glVertex3f(-0.1, 3, -2),
+    glVertex3f(0.1, 3, -2),
+    glVertex3f(0.1, 0, -1.5),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 0, 1.5),
+    glVertex3f(0.1, 0, 1.5),
+    glVertex3f(0.1, 0, -1.5),
+    glVertex3f(-0.1, 0, -1.5),
+    glEnd()
+    glRotatef(-direcao, 0, 1, 0)
+    glRotatef(10, 0, 0, 1)
+    glTranslatef(-1.85, -1.25, 0)
+    #asa traseira vertical direita
+    glTranslatef(-1.85, 1.25, 0)
+    glRotatef(10, 0, 0, 1)
+    glRotatef(direcao, 0, 1, 0)
+    glColor3f(0.4, 0.4, 0.4)
+    glBegin(GL_POLYGON)
+    glVertex3f(0.1, 0, 1.5),
+    glVertex3f(0.1, 0, -1.5),
+    glVertex3f(0.1, 3, -2),
+    glVertex3f(0.1, 3, -0.5),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 0, 1.5),
+    glVertex3f(-0.1, 0, -1.5),
+    glVertex3f(-0.1, 3, -2),
+    glVertex3f(-0.1, 3, -0.5),
+    glEnd()
+    glColor3f(0.3, 0.3, 0.3)
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 0, 1.5),
+    glVertex3f(-0.1, 3, -0.5),
+    glVertex3f(0.1, 3, -0.5),
+    glVertex3f(0.1, 0, 1.5),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 3, -0.5),
+    glVertex3f(0.1, 3, -0.5),
+    glVertex3f(0.1, 3, -2),
+    glVertex3f(-0.1, 3, -2),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 0, -1.5),
+    glVertex3f(-0.1, 3, -2),
+    glVertex3f(0.1, 3, -2),
+    glVertex3f(0.1, 0, -1.5),
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-0.1, 0, 1.5),
+    glVertex3f(0.1, 0, 1.5),
+    glVertex3f(0.1, 0, -1.5),
+    glVertex3f(-0.1, 0, -1.5),
+    glEnd()
+    glRotatef(-direcao, 0, 1, 0)
+    glRotatef(-10, 0, 0, 1)
+    glTranslatef(1.85, -1.25, 0)
+
+    glTranslatef(0, 0, 7)
+
+    glTranslatef(0, -subir, 0)
+    
+
+    
+     #----------------------------------------------------------------------------------------------
+    
+    # Cenário > #pista
+    glColor3f(0.13, 0.13, 0.14)
+    glBegin(GL_POLYGON)
+    glVertex3f(-20, -4.7, -130)
+    glVertex3f(-20, -4.7, 100)
+    glVertex3f(20, -4.7, 100)
+    glVertex3f(20, -4.7, -130)
+    glEnd()
+    
+    glColor3f(0.9, 0.9, 0.9)
+    glBegin(GL_POLYGON)
+    glVertex3f(20, -4.7, 100)
+    glVertex3f(20, -4.2, 100)
+    glVertex3f(20, -4.2, -130)
+    glVertex3f(20, -4.7, -130)
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(20, -4.2, 100)
+    glVertex3f(22, -4.2, 100)
+    glVertex3f(22, -4.2, -130)
+    glVertex3f(20, -4.2, -130)
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-20, -4.7, 100)
+    glVertex3f(-20, -4.2, 100)
+    glVertex3f(-20, -4.2, -130)
+    glVertex3f(-20, -4.7, -130)
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-20, -4.2, 100)
+    glVertex3f(-22, -4.2, 100)
+    glVertex3f(-22, -4.2, -130)
+    glVertex3f(-20, -4.2, -130)
+    glEnd()
+
+    glColor3f(0.8, 0.8, 0.8)
+    glBegin(GL_POLYGON)
+    glVertex3f(-1, -4.699, (faixa1-24))
+    glVertex3f(-1, -4.699, faixa1)
+    glVertex3f(1, -4.699, faixa1)
+    glVertex3f(1, -4.699, (faixa1-24))
+    glEnd()
+    glBegin(GL_POLYGON)
+    glVertex3f(-1, -4.699, (faixa2-24))
+    glVertex3f(-1, -4.699, faixa2)
+    glVertex3f(1, -4.699, faixa2)
+    glVertex3f(1, -4.699, (faixa2-24))
+    glEnd()
+    
+    # < Cenário
+    
+    #----------------------------------------------------------------------------------------------
+    glRotatef(-px, 1, 0, 0)
+    glRotatef(-py, 0, 1, 0)
+    glRotatef(-pz, 0, 0, 1)
+    
+    glFlush()
+
+def buttons(key,x,y):
+    global px, py, pz
+    if key == GLUT_KEY_LEFT:
+        py -= 3
+        
+    elif key == GLUT_KEY_RIGHT:
+        py += 3
+        
+    elif key == GLUT_KEY_UP:
+        px += 3
+        
+    elif key == GLUT_KEY_DOWN:
+        px -= 3
+    
+    elif key == GLUT_KEY_PAGE_UP:
+        pz -= 3
+    
+    elif key == GLUT_KEY_PAGE_DOWN:
+        pz += 3
+    
+    elif key == GLUT_KEY_HOME: # # Voltar para a vista frental (tecla Home)
+        px = 0
+        py = 0
+        pz = 0
+        
+    elif key == GLUT_KEY_END: # Vista em perspectiva (tecla End)
+        px = 6
+        py = -30
+        pz = 0
+        
+    glutPostRedisplay()
+    
+def movimento(key,x,y):
+    global direcao, faixa1, faixa2, rotacaoy, subir
+    
+    if (key == b'w') or (key == b'W'):
+        faixa1 -= 1
+        faixa2 -= 1
+        
+        if (rotacaoy > 1) and ( subir < 8):
+            subir += rotacaoy *0.005
+
+        if (rotacaoy < 1) and ( subir > 0):
+            subir -= -rotacaoy *0.005
+
+        if faixa1 < -80:
+            faixa1 = 80
+
+        if faixa2 < -80:
+            faixa2 = 80
+    
+    elif (key == b'4'):
+        if direcao < 15:
+            direcao += 3
+    
+    elif (key == b'6'):
+        if direcao > -15:
+            direcao -= 3
+
+    elif (key == b'5'):
+        if rotacaoy < 20:
+            rotacaoy +=2
+    
+    elif (key == b'8'):
+        if rotacaoy > -20:
+            rotacaoy -=2
+    
+    glutPostRedisplay()
+
+def main():
+    global distancia
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(2000,1300)
+    glutInitWindowPosition(0, 0)
+    glutCreateWindow(name)
+    glClearColor(0.42, 0.42, 1, 1) #backgroundcolor
+    glShadeModel(GL_SMOOTH)
+    glFrontFace(GL_CCW)
+    glEnable(GL_DEPTH_TEST)
+    glutDisplayFunc(display)
+    glutSpecialFunc(buttons)
+    glutKeyboardFunc(movimento)
+    glMatrixMode(GL_PROJECTION)
+    gluPerspective(50,(1000/650),1,200)
+    glMatrixMode(GL_MODELVIEW)
+    gluLookAt(8.25,-12.25,20,0,0,0,-0.07,1,0)
+    glPushMatrix()
+    glutMainLoop()
+    return
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glPushMatrix()
+    glRotatef(30,1,1,0)
+    drawF35()
+    glPopMatrix()
+    glutSwapBuffers()
+    return
+
+if __name__ == '__main__': main()
--- python/compGrafica/bim02/p2/main.py
+++ python/compGrafica/bim02/p2/main.py
@@ -2,71 +2,122 @@ import pygame
 from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from OpenGL.GLUT import *
 import random
+import math
 
-# Desenhar o corpo principal da nave como um disco simples
-def draw_ship_body():
-    glPushMatrix()
-    glColor3f(1.0, 0.0, 0.0)  # Cor vermelha para o corpo principal
-    glutSolidCylinder(1.5, 0.2, 50, 10)  # Base da nave em formato de cilindro achatado
-    glPopMatrix()
+# Variáveis de controle para o movimento da nave
+posicao_nave = -5
+dash_iniciado = False
 
-# Desenhar naceles de dobra como cubos simples
-def draw_nacelles():
-    glPushMatrix()
-    glColor3f(0.0, 1.0, 0.0)  # Cor verde para naceles
-    glTranslatef(-2.0, 0.0, -1.0)
-    glScalef(0.2, 0.7, 0.2)
-    glutSolidCube(1)
-    glPopMatrix()
+def desenhar_corpo_principal(raio_x, raio_y, profundidade, n_lados, n_camadas):
+    """Desenha uma cúpula convexa mais grossa para o corpo principal da nave."""
+    for i in range(n_camadas + 1):
+        camada_profundidade = profundidade * (1 - (i / n_camadas) ** 2)  # Aumentando a profundidade
+        raio_atual_x = raio_x * (1 - i / n_camadas)
+        raio_atual_y = raio_y * (1 - i / n_camadas)
+        
+        glBegin(GL_TRIANGLE_FAN)
+        glColor3f(0.6, 0.6, 1)
+        glVertex3f(0, 0, camada_profundidade / 2)
+        for j in range(n_lados + 1):
+            angle = 2 * math.pi * j / n_lados
+            x = raio_atual_x * math.cos(angle)
+            y = raio_atual_y * math.sin(angle)
+            glVertex3f(x, y, camada_profundidade / 2)
+        glEnd()
 
-    glPushMatrix()
-    glTranslatef(2.0, 0.0, -1.0)
-    glScalef(0.2, 0.7, 0.2)
-    glutSolidCube(1)
-    glPopMatrix()
+def desenhar_cauda():
+    """Desenha uma cauda conectada ao corpo principal da nave."""
+    glBegin(GL_QUADS)
+    glColor3f(0.8, 0.2, 0.2)  # Vermelho escuro para a cauda
 
-# Desenhar estrelas no fundo
-def draw_stars():
-    glBegin(GL_POINTS)
-    glColor3f(1.0, 1.0, 1.0)  # Cor branca para as estrelas
-    for _ in range(100):
-        glVertex3f(random.uniform(-15, 15), random.uniform(-10, 10), random.uniform(-20, -5))
+    # Ajustando a posição da cauda para que fique conectada ao corpo principal
+    altura_conexao = 0  # Valor aumentado para conectar mais a cauda ao corpo
+
+    # Parte da cauda conectada ao corpo
+    glVertex3f(-0.05,altura_conexao, -0.05)
+    glVertex3f(0.05, altura_conexao, -0.05)
+    glVertex3f(0.5,  altura_conexao, -0.5)
+    glVertex3f(-0.05,  altura_conexao, -0.5)
+
+    # Parte traseira da cauda mais estreita
+    glVertex3f(-0.03, altura_conexao, -0.5)
+    glVertex3f(0.03,  altura_conexao, -0.5)
+    glVertex3f(0.03, altura_conexao, -1.0)
+    glVertex3f(-0.03,  altura_conexao, -1.0)
+
+    # Laterais da cauda para um visual mais detalhado
+    glVertex3f(-0.05,  altura_conexao, -0.05)
+    glVertex3f(-0.05, 0.02 + altura_conexao, -0.05)
+    glVertex3f(-0.03, 0.02 + altura_conexao, -0.5)
+    glVertex3f(-0.03,  altura_conexao, -0.5)
+
+    glVertex3f(0.05, altura_conexao, -0.05)
+    glVertex3f(0.05, 0.02 + altura_conexao, -0.05)
+    glVertex3f(0.03, 0.02 + altura_conexao, -0.5)
+    glVertex3f(0.03, altura_conexao, -0.5)
+
     glEnd()
 
-# Inicializar a cena e a perspectiva
-def init_scene():
-    glEnable(GL_DEPTH_TEST)
-    glClearColor(0.0, 0.0, 0.2, 1.0)  # Fundo espacial escuro
-    gluPerspective(45, (800/600), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -10)  # Ajuste a posição da câmera mais próxima
+def gerar_estrelas(qtd_estrelas):
+    """Gera uma lista de posições de estrelas no fundo."""
+    estrelas = []
+    for _ in range(qtd_estrelas):
+        x = random.uniform(-10, 10)
+        y = random.uniform(-10, 10)
+        z = random.uniform(-20, -5)  # Fundo mais distante
+        estrelas.append((x, y, z))
+    return estrelas
 
-# Função principal
+def desenhar_estrelas(estrelas):
+    """Desenha as estrelas no fundo."""
+    glBegin(GL_POINTS)
+    glColor3f(1, 1, 1)
+    for estrela in estrelas:
+        glVertex3f(*estrela)
+    glEnd()
+
 def main():
+    global posicao_nave, dash_iniciado
+
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    init_scene()
-    glutInit()  # Inicializar GLUT
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
 
-    running = True
-    while running:
+    # Gerar estrelas para o fundo
+    estrelas = gerar_estrelas(100)
+
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+        # Desenha as estrelas ao fundo
+        desenhar_estrelas(estrelas)
 
-        # Desenhar elementos diretamente, sem rotação ou inclinação
-        draw_stars()
-        draw_ship_body()
-        draw_nacelles()
+        # Posiciona e desenha a nave
+        glPushMatrix()
+        glTranslatef(0.0, 0.0, posicao_nave)  # Controla a posição da nave
+        glRotatef(90, 1, 0, 0)  # Deita o corpo principal
+        glRotatef(180, 0, 1, 0)  # Inverte a nave para deixar a cauda para baixo
+        desenhar_corpo_principal(raio_x=0.8, raio_y=0.8, profundidade=0.3, n_lados=50, n_camadas=10)  # Aumentei a profundidade
+        desenhar_cauda()
+        glPopMatrix()
+
+        # Atualiza a posição da nave
+        if not dash_iniciado:
+            posicao_nave += 0.02  # Movimento lento da nave
+            if posicao_nave >= -2:  # Inicia o dash quando atinge um ponto
+                dash_iniciado = True
+        else:
+            posicao_nave += 0.5  # Dash rápido para fora da tela
 
         pygame.display.flip()
-        pygame.time.Clock().tick(60)
-
-    pygame.quit()
+        pygame.time.wait(10)
 
 if __name__ == "__main__":
     main()
